ac46423d503a593698eab703f46a81693a2a3596
Jana Schmälzle <jana@familie-schmaelzle.de>
2023-07-06 15:17:15 +0200

    fix Show-Answer-Button, add bookmark function

--- js/index.js
+++ js/index.js
@@ -3,28 +3,24 @@ console.clear();
 //Bookmark-setzen
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 
-console.log(bookmarkButton);
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark-active");
 });
 
 // Show/ Hide Answer- Button
 
-/* 
+const answerButton = document.querySelector('[data-js="answer-button"]');
 const cardAnswer = document.querySelector('[data-js="card-answer"]');
 
 answerButton.addEventListener("click", () => {
-  cardAnswer.classList.toggle
-}) */
-const answerButton = document.querySelector('[data-js="answer-button"]');
-
-function showAnswer(element) {
-  var x = document.getElementById(element);
-  if (x.style.display != "block") {
-    x.style.display = "block";
+  if (
+    cardAnswer.style.display === "none" &&
+    answerButton.textContent == "Show Answer"
+  ) {
+    cardAnswer.style.display = "block";
     answerButton.textContent = "Hide Answer";
   } else {
-    x.style.display = "none";
+    cardAnswer.style.display = "none";
     answerButton.textContent = "Show Answer";
   }
-}
+});
