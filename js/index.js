b5f08cd2a483479a328dfbba2c2c3320eff9566f
Jana Schmälzle <jana@familie-schmaelzle.de>
2023-07-06 12:25:46 +0200

    add bookmark functionality, show answer

--- js/index.js
+++ js/index.js
@@ -1,9 +1,30 @@
 console.clear();
 
-const bodyElement = document.querySelector('[data-js="body"]');
+//Bookmark-setzen
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 
-const toggleMode = document.querySelector('[data-js="toggle-button"]');
-
-toggleMode.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
+console.log(bookmarkButton);
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark-active");
 });
+
+// Show/ Hide Answer- Button
+
+/* 
+const cardAnswer = document.querySelector('[data-js="card-answer"]');
+
+answerButton.addEventListener("click", () => {
+  cardAnswer.classList.toggle
+}) */
+const answerButton = document.querySelector('[data-js="answer-button"]');
+
+function showAnswer(element) {
+  var x = document.getElementById(element);
+  if (x.style.display != "block") {
+    x.style.display = "block";
+    answerButton.textContent = "Hide Answer";
+  } else {
+    x.style.display = "none";
+    answerButton.textContent = "Show Answer";
+  }
+}
